96becddf551d50046b89ba52d19ca0c76dca4359
CambridgeinstitutetechnolgyBLR-Harshini <harshini.24ece@cambridge.edu.in>
2026-05-26 14:11:45 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -8,6 +8,63 @@ from cocotb.triggers import ClockCycles
 
 @cocotb.test()
 async def test_project(dut):
+    """Test A == B"""
+    dut.ui_in.value  = 0b10101010   # A = 170
+    dut.uio_in.value = 0b10101010   # B = 170
+    dut.ena.value = 1
+    dut.rst_n.value = 1
+    await Timer(10, units="ns")
+
+    assert dut.uo_out.value & 0x01, "EQ flag should be 1"
+    assert not (dut.uo_out.value & 0x02), "GT flag should be 0"
+    assert not (dut.uo_out.value & 0x04), "LT flag should be 0"
+
+@cocotb.test()
+async def test_comparator_greater(dut):
+    """Test A > B"""
+    dut.ui_in.value  = 200   # A
+    dut.uio_in.value = 100   # B
+    dut.ena.value = 1
+    dut.rst_n.value = 1
+    await Timer(10, units="ns")
+
+    assert not (dut.uo_out.value & 0x01), "EQ should be 0"
+    assert dut.uo_out.value & 0x02,       "GT should be 1"
+    assert not (dut.uo_out.value & 0x04), "LT should be 0"
+
+@cocotb.test()
+async def test_comparator_less(dut):
+    """Test A < B"""
+    dut.ui_in.value  = 50    # A
+    dut.uio_in.value = 200   # B
+    dut.ena.value = 1
+    dut.rst_n.value = 1
+    await Timer(10, units="ns")
+
+    assert not (dut.uo_out.value & 0x01), "EQ should be 0"
+    assert not (dut.uo_out.value & 0x02), "GT should be 0"
+    assert dut.uo_out.value & 0x04,       "LT should be 1"
+
+@cocotb.test()
+async def test_comparator_random(dut):
+    """Random tests"""
+    dut.ena.value = 1
+    dut.rst_n.value = 1
+
+    for _ in range(20):
+        a = random.randint(0, 255)
+        b = random.randint(0, 255)
+        dut.ui_in.value  = a
+        dut.uio_in.value = b
+        await Timer(10, units="ns")
+
+        eq = (dut.uo_out.value & 0x01) != 0
+        gt = (dut.uo_out.value & 0x02) != 0
+        lt = (dut.uo_out.value & 0x04) != 0
+
+        assert eq == (a == b), f"EQ wrong for A={a} B={b}"
+        assert gt == (a  > b), f"GT wrong for A={a} B={b}"
+        assert lt == (a  < b), f"LT wrong for A={a} B={b}"
     dut._log.info("Start")
 
     # Set the clock period to 10 us (100 KHz)
